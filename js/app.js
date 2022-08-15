8f7db8313d4362216c5572af3516a51495250871
IsraelImmanuel <isrl4824@yahoo.com>
2022-08-15 16:04:53 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -62,8 +62,8 @@ fetch(urlAPI)
       <p class="address">${city}</p>
       <hr />
       <p>${phone}</p>
-      <p class="address">${street}, ${state} ${postcode}</p>
-      <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+      <p class="address">${street}, ${state}, ${postcode}</p>
+      <p>Birthday: ${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}</p>
       </div>
       `;
 
